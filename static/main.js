0abd4340cf2feb9e385e6b07e59439bef39357d0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-02-20 11:40:51 +0530

    Use experimental wikimedia translation system based on nllb

--- static/main.js
+++ static/main.js
@@ -1,7 +1,6 @@
-/*
 function doTranslate(from, to) {
     document.getElementById('progress').style.display = "block";
-    fetch('/api/translate', {
+    fetch('https://translate.wmcloud.org/api/translate', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -14,31 +13,6 @@ function doTranslate(from, to) {
     }).then(response => response.json())
         .then(result => {
             document.getElementById('target_content').innerText = result.translation
-            document.getElementById('progress').style.display = "none";
-        })
-}*/
-
-function doTranslate(from, to) {
-    document.getElementById('progress').style.display = "block";
-
-    const lang_map = {
-        "en": 'eng_Latn',
-        "ml": 'mal_Mlym'
-    }
-    const text = document.getElementById('source_content').value;
-    const payload = {
-        data : [text, lang_map[from], lang_map[to], 1000]
-    }
-    fetch('https://santhosh-nllb-translator.hf.space/api/predict', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    }).then(response => response.json())
-        .then(result => {
-            console.dir(result);
-            document.getElementById('target_content').innerText = result.data[0]
             document.getElementById('progress').style.display = "none";
         })
 }
