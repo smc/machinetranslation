41ea9601aa3ed1ca47cd6c4c38a603f50412fe60
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-03-14 15:33:58 +0530

    Fix the API path and params

--- static/main.js
+++ static/main.js
@@ -1,13 +1,11 @@
 function doTranslate(from, to) {
     document.getElementById('progress').style.display = "block";
-    fetch('https://translate.wmcloud.org/api/translate', {
+    fetch(`https://translate.wmcloud.org/api/translate/${from}/${to}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            from,
-            to,
             text: document.getElementById('source_content').value
         })
     }).then(response => response.json())
