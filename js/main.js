64769169a75ce40101c4bdd9dd934b60b5b22793
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-07-14 12:27:03 +0530

    Update to static site that uses MinT

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 function doTranslate(from, to) {
     document.getElementById('progress').style.display = "block";
+    document.getElementById(`${to}_content`).innerText = '';
     fetch(`https://translate.wmcloud.org/api/translate/${from}/${to}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            text: document.getElementById('source_content').value
+            text: document.getElementById(`${from}_content`).value
         })
     }).then(response => response.json())
         .then(result => {
-            document.getElementById('target_content').innerText = result.translation
+            document.getElementById(`${to}_content`).innerText = result.translation
             document.getElementById('progress').style.display = "none";
         })
 }
