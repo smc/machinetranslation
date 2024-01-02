67c4a67980885473ac0fc710042969eb111b6828
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2024-01-02 13:41:28 +0530

    use new api

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 function doTranslate(from, to) {
     document.getElementById('progress').style.display = "block";
     document.getElementById(`${to}_content`).innerText = '';
-    fetch(`https://translate.wmcloud.org/api/translate/${from}/${to}`, {
+    const content = document.getElementById(`${from}_content`).value;
+    fetch(`https://translate.wmcloud.org/api/translate`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            text: document.getElementById(`${from}_content`).value
+            source_language: from,
+            target_language: to,
+            format: 'text',
+            content: content
         })
-    }).then(response => response.json())
-        .then(result => {
-            document.getElementById(`${to}_content`).innerText = result.translation
-            document.getElementById('progress').style.display = "none";
-        })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Server returned ' + response.status);
+        }
+        return response.json();
+    }).then(result => {
+        document.getElementById(`${to}_content`).innerText = result.translation
+        document.getElementById('progress').style.display = "none";
+    })
+    .catch(error => {
+        // Error handling
+        console.error('An error occurred:', error);
+        // Display an error message to the user
+        document.getElementById('status').innerText = 'An error occurred. Please try again.';
+    }).finally(()=>{
+        document.getElementById('progress').style.display = "none";
+    });
 }
